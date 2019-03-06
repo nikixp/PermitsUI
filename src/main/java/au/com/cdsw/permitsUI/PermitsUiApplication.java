6243cdcdb63146d087cb5d3dc7d9dc5ba3771883
App-Teck <appteck01@gmail.com>
2019-03-06 13:57:13 +1100

    first

---
+++ src/main/java/au/com/cdsw/permitsUI/PermitsUiApplication.java
@@ -0,0 +1,13 @@
+package au.com.cdsw.permitsUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermitsUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermitsUiApplication.class, args);
+	}
+
+}
