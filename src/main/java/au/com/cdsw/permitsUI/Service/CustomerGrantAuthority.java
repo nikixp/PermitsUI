86cec9a4e51316bc186ad49c6fe103188637ff4b
NBHristov <nikixp8@gmail.com>
2019-03-20 10:04:33 +0200

    fixed customer pojo and AuthController

--- src/main/java/au/com/cdsw/permitsUI/Service/CustomerGrantAuthority.java
+++ src/main/java/au/com/cdsw/permitsUI/Service/CustomerGrantAuthority.java
@@ -1,37 +1,41 @@
 package au.com.cdsw.permitsUI.Service;
 
+import au.com.cdsw.permitsUI.Entity.customer.CustomerAuthority;
 import org.springframework.security.core.GrantedAuthority;
+
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 public class CustomerGrantAuthority implements GrantedAuthority {
 
-    private String customerJson;
+    private String customerEmail;
+    private CustomerAuthority customerAuthority;
 
-    public CustomerGrantAuthority(String customerJson){
-        this.customerJson = customerJson;
+    public CustomerGrantAuthority(@NotNull CustomerAuthority customerAuthority){
+        this.customerAuthority = customerAuthority;
+        this.customerEmail = customerAuthority.getCustomer().getEmail();
+    }
+
+    public CustomerAuthority getCustomerAuthority() {
+        return customerAuthority;
     }
 
     @Override
     public String getAuthority() {
-        return customerJson;
+        return customerEmail;
     }
 
     @Override
-    public boolean equals(Object o){
-
-        if(this == o) return true;
-        if(o == null || getClass() != o.getClass()) return false;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         CustomerGrantAuthority that = (CustomerGrantAuthority) o;
-        return java.util.Objects.equals(customerJson, this.customerJson);
+        return Objects.equals(customerEmail, that.customerEmail);
     }
 
     @Override
-    public int hashCode(){
-        return java.util.Objects.hash(customerJson);
-    }
-
-    @Override
-    public String toString(){
-        return this.customerJson;
+    public int hashCode() {
+        return Objects.hash(customerEmail);
     }
 
 }
